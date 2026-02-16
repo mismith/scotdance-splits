95bffdec4ec482ce862b9be0e47cc3e4170f14d7
Murray Rowan <murray@mismith.info>
2026-02-15 22:30:37 -0700

    build: add analyze script for bundle visualization

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { URL, fileURLToPath } from 'node:url'
 
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), process.env.ANALYZE && visualizer({ open: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
