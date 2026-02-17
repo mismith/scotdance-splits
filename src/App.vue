8d1e8111242c0e24750fad856f666d882b794986
Murray Rowan <murray@mismith.info>
2026-02-16 21:47:20 -0700

    fix: allow long-tapping drag handles before dragging

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import BackgroundTexture from '@/assets/bg/txture.png?url'
 import { TooltipProvider } from '@/components/ui/tooltip'
 </script>
 
 <template>
   <TooltipProvider>
-    <RouterView :style="{ backgroundImage: `url(${BackgroundTexture})` }" />
+    <RouterView />
   </TooltipProvider>
 </template>
