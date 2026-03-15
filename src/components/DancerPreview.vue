450202370c164f8ae3bd2e1cd71d3888d7dbcfaf
Murray Rowan <murray@mismith.info>
2026-03-15 00:19:04 -0600

    feat: remove bib numbers from pre-export-dialog UI

--- src/components/DancerPreview.vue
+++ src/components/DancerPreview.vue
@@ -13,12 +13,7 @@ defineProps<{
 
 <template>
   <ul class="space-y-1 list-none p-0 m-0">
-    <li v-for="dancer in dancers" :key="dancer.bibNumber" class="flex items-center gap-2 text-xs">
-      <span
-        class="bg-primary/20 text-primary px-1.5 py-0.5 rounded text-[10px] font-bold min-w-[2rem] text-center"
-      >
-        {{ dancer.bibNumber }}
-      </span>
+    <li v-for="(dancer, i) in dancers" :key="i" class="flex items-center gap-2 text-xs">
       <span class="truncate text-foreground flex-1"
         >{{ dancer.firstName }} {{ dancer.lastName }}</span
       >
